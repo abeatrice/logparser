38ad1ed1bbbc753a647f49c05d6bd09d591fafe1
andrew beatrice <abeatrice.mail@gmail.com>
2021-02-18 09:55:46 -0800

    add geoIp2

--- src/Services/Parser.php
+++ src/Services/Parser.php
@@ -4,17 +4,20 @@ namespace App\Services;
 
 use Symfony\Component\Filesystem\Filesystem;
 use Kassner\LogParser\LogParser;
+use App\Services\GeoIp2;
 
 class Parser
 {
     private $logParser;
     private $file;
     private $fileSystem;
+    private $geoIp2;
 
     public function __construct($file)
     {
         $this->file = $file;
         $this->fileSystem = new Filesystem();
+        $this->geoIp2 = new GeoIp2();
         $this->logParser = new LogParser();
         $this->logParser->setFormat('%h %l %u %t "%r" %>s %O "%{Referer}i" \"%{User-Agent}i"');
     }
@@ -28,7 +31,9 @@ class Parser
         foreach ($lines as $line) {
             try {
                 $entry = $this->logParser->parse($line);
-                var_dump($entry);
+                list($country, $state) = $this->geoIp2->getGeo($entry->host);
+                var_dump($entry->host);
+                var_dump($country,$state);
             } catch (\Throwable $th) {
                 var_dump($th->getMessage());
             }
